d24afb19d44c881ebb55efdbcec61ec33e1c667b
mindwing <mindwing@gmail.com>
2015-04-14 16:39:27 +0900

    toast

--- src/com/example/camp_exam_webbrowser/MainActivity.java
+++ src/com/example/camp_exam_webbrowser/MainActivity.java
@@ -2,15 +2,55 @@ package com.example.camp_exam_webbrowser;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnKeyListener;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+	private EditText textUrl;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		textUrl = (EditText) findViewById(R.id.text_url);
+		textUrl.setOnKeyListener(new OnKeyListener() {
+
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				if (event.getAction() != KeyEvent.ACTION_DOWN) {
+					return false;
+				}
+
+				boolean processed = false;
+
+				switch (keyCode) {
+				case KeyEvent.KEYCODE_ENTER:
+					Toast.makeText(
+							MainActivity.this,
+							textUrl.getText().toString() + ": "
+									+ event.getAction(), Toast.LENGTH_SHORT)
+							.show();
+
+					processed = true;
+
+					break;
+
+				default:
+
+					break;
+				}
+
+				return processed;
+			}
+
+		});
 	}
 
 	@Override
@@ -31,4 +71,5 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
 }
